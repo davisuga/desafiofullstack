b32b8efca911365ef13fb35184edec87f914cd46
Davi Suga <daviciencia1@gmail.com>
2021-03-16 12:46:41 -0300

    feat(graphql.ts): added missing typedefs and typings.

--- global.d.ts
+++ global.d.ts
@@ -9,3 +9,42 @@ type StackoverflowApiParams = {
   fromdate: number;
   todate: number;
 };
+
+type GraphQlApiParams = {
+  score: number;
+  sort: "activity" | "votes" | "creation" | "hot" | "week" | "created";
+  limit: number;
+  tags: string;
+};
+
+type QuestionResponse = {
+  items: Item[];
+  has_more: boolean;
+  quota_max: number;
+  quota_remaining: number;
+};
+
+type Item = {
+  score: number;
+  link: string;
+  title: string;
+  owner: Owner;
+  question_id: number;
+  tags: string[];
+  view_count: number;
+  answer_count: number;
+  creation_date: number;
+  last_edit_date: number;
+  is_answered: boolean;
+  content_license: string;
+  last_activity_date: number;
+};
+
+type Owner = {
+  reputation: number;
+  user_id: number;
+  user_type: string;
+  profile_image: string;
+  display_name: string;
+  link: string;
+};
--- pages/api/graphql.ts
+++ pages/api/graphql.ts
@@ -1,28 +1,48 @@
-import { ApolloServer, gql } from 'apollo-server-micro'
+import { ApolloServer, gql } from "apollo-server-micro";
 
 const typeDefs = gql`
   type Query {
-    users: [User!]!
+    questions(tag: String!, limit: Int, score: Int, sort: String): [Question!]!
   }
-  type User {
-    name: String
+  type Owner {
+    reputation: Int
+    user_id: ID!
+    user_type: String
+    profile_image: String
+    display_name: String!
+    link: String!
   }
-`
+  type Question {
+    score: Int!
+    link: String!
+    title: String!
+    owner: Owner!
+    question_id: ID!
+    tags: [String!]!
+    view_count: Int!
+    answer_count: Int!
+    creation_date: Date!
+    last_edit_date: Date
+    is_answered: Boolean!
+    last_activity_date: Date!
+  }
+  scalar Date
+`;
 
 const resolvers = {
   Query: {
-    users(parent, args, context) {
-      return [{ name: 'Nextjs' }]
+    questions(parent, args, context) {
+      return [{ name: "Nextjs" }];
     },
   },
-}
+};
 
-const apolloServer = new ApolloServer({ typeDefs, resolvers })
+const apolloServer = new ApolloServer({ typeDefs, resolvers });
 
 export const config = {
   api: {
     bodyParser: false,
   },
-}
+};
 
-export default apolloServer.createHandler({ path: '/api/graphql' })
+export default apolloServer.createHandler({ path: "/api/graphql" });
