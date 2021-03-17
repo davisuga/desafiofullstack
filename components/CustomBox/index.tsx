184060558a2fa1bc6104d40fa157b8dfa2ddd5aa
Davi Suga <daviciencia1@gmail.com>
2021-03-17 14:20:02 -0300

    fix(typings) fixed theme typings

--- components/CustomBox/index.tsx
+++ components/CustomBox/index.tsx
@@ -1,12 +1,9 @@
 import { Box, useStyleConfig, ChakraComponent } from "@chakra-ui/react";
-import { variants } from "../../theme/components/box";
 import { ComponentProps } from "react";
 
 type BoxProps = ComponentProps<ChakraComponent<"div", {}>>;
-interface CustomBoxProps extends BoxProps {
-  variant?: keyof variants;
-}
-function CustomBox(props: CustomBoxProps) {
+
+function CustomBox(props: BoxProps) {
   const { size, variant, ...rest } = props;
   const styles = useStyleConfig("Box", { size, variant });
   return <Box sx={styles} {...rest} />;
