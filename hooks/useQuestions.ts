1452e1d0c59c1064b15be081a98977f1ba95794e
Davi Suga <daviciencia1@gmail.com>
2021-03-17 10:38:03 -0300

    feat(useQuestions): created hook do fetch data from the graphql api.

---
+++ hooks/useQuestions.ts
@@ -0,0 +1,44 @@
+import { DocumentNode } from "graphql";
+import { useState, useEffect } from "react";
+import client from "../services/apollo";
+
+const useQuestions = (
+  initialQuery?: DocumentNode,
+  initialVariables?: GraphQlApiParams
+) => {
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+  const [questions, setQuestions] = useState([]);
+
+  useEffect(() => {}, []);
+
+  const getQuestions = async (
+    query = initialQuery,
+    variables = initialVariables
+  ) => {
+    setLoading(true);
+    try {
+      const results = await client.query<GraphQLApiResponse>({
+        query,
+        variables,
+      });
+      console.log(
+        "usequestions got: ",
+        results,
+        " from: ",
+        query,
+        " variables: ",
+        variables
+      );
+      setQuestions(results.data.questions);
+    } catch (error) {
+      setError(error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return { getQuestions, questions, loading, error };
+};
+
+export default useQuestions;
