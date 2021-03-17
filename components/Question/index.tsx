ff98efc35c21dc26f3a8444f1d9ce310a98c3534
Davi Suga <daviciencia1@gmail.com>
2021-03-17 13:18:11 -0300

    feat(question): added creation data
    Modified graphql query in order to obtain the creation date

--- components/Question/index.tsx
+++ components/Question/index.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Box from "../CustomBox";
-import { Image, Text } from "@chakra-ui/react";
+import { Image, Text, HStack } from "@chakra-ui/react";
 // import { Container } from './styles';
 type QuestionProps = { question: Item };
 const Question = ({ question }: QuestionProps) => {
@@ -16,7 +16,12 @@ const Question = ({ question }: QuestionProps) => {
       </Box>
       <Box variant="question-content-wrapper">
         <Box variant="question-content">
-          <Text fontWeight="bold">{question.owner.display_name}</Text>
+          <HStack>
+            <Text fontWeight="bold">{question.owner.display_name}</Text>
+            <Text color="gray" size="sm">
+              {new Date(question.creation_date * 1000).toDateString()}
+            </Text>
+          </HStack>
           <h1>{question.title}</h1>
         </Box>
         <Box variant="question-info"></Box>
