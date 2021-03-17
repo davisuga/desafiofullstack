25739f364d394a78deeb8e94083aa22058878fb3
Davi Suga <daviciencia1@gmail.com>
2021-03-17 14:01:03 -0300

    feat(question) added hover animation to the component

--- components/Question/index.tsx
+++ components/Question/index.tsx
@@ -1,32 +1,35 @@
 import React from "react";
 import Box from "../CustomBox";
 import { Image, Text, HStack } from "@chakra-ui/react";
+import MotionBox from "../MotionBox";
 // import { Container } from './styles';
 type QuestionProps = { question: Item };
 const Question = ({ question }: QuestionProps) => {
   return (
-    <Box variant="question">
-      <Box variant="profile-image">
-        <Image
-          objectFit="scale-down"
-          borderRadius="full"
-          src={question.owner.profile_image}
-          height="10"
-        />
-      </Box>
-      <Box variant="question-content-wrapper">
-        <Box variant="question-content">
-          <HStack>
-            <Text fontWeight="bold">{question.owner.display_name}</Text>
-            <Text color="gray" size="sm">
-              {new Date(question.creation_date * 1000).toDateString()}
-            </Text>
-          </HStack>
-          <h1>{question.title}</h1>
+    <MotionBox whileHover={{ scale: 1.03 }}>
+      <Box variant="question">
+        <Box variant="profile-image">
+          <Image
+            objectFit="scale-down"
+            borderRadius="full"
+            src={question.owner.profile_image}
+            height="10"
+          />
         </Box>
-        <Box variant="question-info"></Box>
+        <Box variant="question-content-wrapper">
+          <Box variant="question-content">
+            <HStack>
+              <Text fontWeight="bold">{question.owner.display_name}</Text>
+              <Text color="gray" size="sm">
+                {new Date(question.creation_date * 1000).toDateString()}
+              </Text>
+            </HStack>
+            <h1>{question.title}</h1>
+          </Box>
+          <Box variant="question-info"></Box>
+        </Box>
       </Box>
-    </Box>
+    </MotionBox>
   );
 };
 
