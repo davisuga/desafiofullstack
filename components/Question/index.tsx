332bbf8bbd278bc5e99760f3f59ff7e5ba7fb9dc
Davi Suga <daviciencia1@gmail.com>
2021-03-17 12:54:02 -0300

    feat(question): implemented styles and organized data in the component

--- components/Question/index.tsx
+++ components/Question/index.tsx
@@ -1,14 +1,27 @@
 import React from "react";
-import CustomBox from "../CustomBox";
-
+import Box from "../CustomBox";
+import { Image, Text } from "@chakra-ui/react";
 // import { Container } from './styles';
 type QuestionProps = { question: Item };
 const Question = ({ question }: QuestionProps) => {
   return (
-    <CustomBox>
-      <h1>{question.title}</h1>
-      <p>from {question.owner.display_name}</p>
-    </CustomBox>
+    <Box variant="question">
+      <Box variant="profile-image">
+        <Image
+          objectFit="scale-down"
+          borderRadius="full"
+          src={question.owner.profile_image}
+          height="10"
+        />
+      </Box>
+      <Box variant="question-content-wrapper">
+        <Box variant="question-content">
+          <Text fontWeight="bold">{question.owner.display_name}</Text>
+          <h1>{question.title}</h1>
+        </Box>
+        <Box variant="question-info"></Box>
+      </Box>
+    </Box>
   );
 };
 
