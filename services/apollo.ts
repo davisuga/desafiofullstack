4d6add0dda239451a13b49e469ec198b2564ce1a
davisuga <daviciencia1@gmail.com>
2022-01-25 18:15:10 -0300

    fixed graphql url

--- services/apollo.ts
+++ services/apollo.ts
@@ -2,9 +2,12 @@ import { ApolloClient, InMemoryCache } from "@apollo/client";
 import "cross-fetch/polyfill";
 
 const cache = new InMemoryCache();
-const uri = process.env.VERCEL_URL
-  ? `${process.env.VERCEL_URL}/api/graphql`
-  : "http://localhost:3000/api/graphql";
+
+const uri =
+  process.env.NODE_ENV !== "development"
+    ? "/api/graphql"
+    : "http://localhost:3000/api/graphql";
+
 export default new ApolloClient({
   // Provide required constructor fields
   cache: cache,
